9d7cd5993f87f0517c3f4ad52838857dd9afa31a
James Thistlewood <jamesthistlewood@gmail.com>
2021-04-10 17:17:57 +0100

    update default sandbox

--- sandbox.py
+++ sandbox.py
@@ -6,8 +6,8 @@ from contestants import example_naiive
 from controller import Controller
 
 ctrl = Controller(
-    [example_random.strategy, example_random.turn],
-    [example_naiive.strategy, example_naiive.turn],
+    [example_random.strategy],
+    [example_naiive.strategy],
 )
 
 # Run games
